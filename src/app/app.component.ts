cc79fc7daa98aacddebc6f867d27917f9890815e
Tyler Trussell <tigertrussell@gmail.com>
2023-06-11 12:40:39 -0500

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,22 @@
 import { Component } from '@angular/core';
+import {
+    FormControl,
+    FormGroup,
+    Validators,
+    ValidatorFn,
+    AbstractControl,
+    ValidationErrors,
+} from '@angular/forms';
+
+const passwordsMatchValidator: ValidatorFn = (control: AbstractControl): ValidationErrors | null => {
+    const password = control.get('password');
+    const passwordConfirm = control.get('passwordConfirm');
+    if (password && passwordConfirm && password.value !== passwordConfirm.value) {
+        return { passwordsDontMatch: true };
+    } else {
+        return null;
+    }
+};
 
 @Component({
   selector: 'app-root',
@@ -6,5 +24,54 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ng-form-minimal-repro';
+    name = new FormControl('', [Validators.required]);
+    email = new FormControl('', [Validators.required, Validators.email]);
+    password = new FormControl('', [Validators.required, Validators.minLength(8), Validators.maxLength(64)]);
+    passwordConfirm = new FormControl('', [Validators.required, Validators.minLength(8), Validators.maxLength(64)]);
+    formGroup = new FormGroup({
+        'name': this.name,
+        'email': this.email,
+        'password': this.password,
+        'passwordConfirm': this.passwordConfirm
+    }, { validators: passwordsMatchValidator });
+
+
+    constructor () {}
+
+    errorMessages: { [key: string]: string; } = {
+        'name': 'Tell us what we should call you',
+        'email': 'A valid email is required for your account',
+        'password': 'Enter an 8 to 64 character password',
+        'passwordsDontMatch': "Passwords don't match",
+    };
+
+    getErrorMessage(fieldName: string) {
+        return this.errorMessages[fieldName];
+    }
+
+    getPasswordConfirmErrorMessage() {
+        let errorMessage = null;
+        console.log(this.passwordConfirm);
+        console.log(this.formGroup);
+        if (this.passwordConfirm &&
+            this.passwordConfirm.errors &&
+            (this.passwordConfirm.errors['required'] || this.passwordConfirm.errors['minlength'] || this.passwordConfirm.errors['maxlength'])) {
+            errorMessage = this.errorMessages['password'];
+        } else if (this.formGroup &&
+            this.formGroup.errors &&
+            this.formGroup.errors['passwordsDontMatch']) {
+            errorMessage = this.errorMessages['passwordsDontMatch'];
+        }
+        console.log("Error message: " + errorMessage);
+        return errorMessage;
+    }
+
+    submitForm() {
+        if (!(this.name.invalid || this.email.invalid || this.password.invalid)) {
+            console.log('success');
+        } else {
+            console.log('invalid');
+        }
+    }
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,12 @@
-import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms'
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +15,14 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MatCardModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
